e5a1981740a03658d8e6ba28db7c02fef45a72b6
Peredreev.Nikita <nik.peredreev@mail.ru>
2024-04-23 22:28:31 +0300

    initial commit

---
+++ Dal/DbModels/DefaultDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Dal.DbModels
+{
+    public partial class DefaultDbContext : DbContext
+    {
+        public DefaultDbContext()
+        {
+        }
+
+        public DefaultDbContext(DbContextOptions<DefaultDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Common.Configuration.SharedConfiguration.DbConnectionString ?? "Data Source=localhost\\SQLEXPRESS;Initial Catalog=NetCoreTemplateDb;Integrated security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Login, "Unique_Users_Login")
+                    .IsUnique();
+
+                entity.Property(e => e.Login)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.Password).IsRequired();
+
+                entity.Property(e => e.RegistrationDate).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
